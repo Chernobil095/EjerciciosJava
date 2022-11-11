04af82486d6177064db1d820c8f4c1cc51374430
luis_ <luis.mejia.p95@gmail.com>
2022-11-11 15:03:00 -0600

    Agregamos las listas dobles, se crearon dos clases, la clase ListaD y la clase NodoD

---
+++ src/NodoD.java
@@ -0,0 +1,15 @@
+public class NodoD <T>{
+    T info;
+    NodoD<T> next;
+    NodoD<T> prev;
+
+    public NodoD(T el){
+        this(el,null,null);
+    }
+
+    public NodoD(T el, NodoD next, NodoD prev){
+        info=el;
+        this.next=next;
+        this.prev=prev;
+    }
+}
---
+++ src/TestListaD.java
@@ -0,0 +1,20 @@
+public class TestListaD {
+    public static void main(String[] args) {
+        ListaD<Double> l=new ListaD();
+        l.insertarFinal(1.3);
+        l.insertarFinal(2.4);
+        l.insertarFinal(3.5);
+        l.insertarFinal(4.0);
+        l.insertarFinal(5.3);
+        l.imprime();
+        System.out.println("eliminado:"+l.eliminarFinal());
+        l.imprime();
+        System.out.println("eliminado"+l.eliminarFrente());
+        l.imprime();
+        System.out.println();
+        System.out.printf("eliminado");
+        l.eliminar(4.0);
+        l.imprime();
+
+    }
+}
